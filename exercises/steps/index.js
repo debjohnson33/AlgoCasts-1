7cf63df808b9efbce3e63e8bb5ac792a5d4cfe57
debjohnson33 <debjohnson_11@yahoo.com>
2018-03-29 23:02:01 -0500

    Finished recursion solution

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,33 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+
+  // for (let i = 0; i < n; i++) { // rows
+  //   let stair = "";
+  //    for (let j = 0; j < n; j++) { // columns
+  //      if (j <= i) {
+  //        stair += "#";
+  //      } else {
+  //        stair += " ";			Solution #1
+  //      }
+  //    }
+  //    console.log(stair);
+  // }
+
+  // Recursion - Solution #2:
+  // Find base case first
+  if (n === row) {
+  	return;
+  }
+  if (n === stair.length) {
+  	console.log(stair); // Do some work
+  	steps(n, row + 1); // Then call function again 
+  }					   // with a change to the variable
+  const add = stair.length <= row ? '#' : ' ';
+  }
+  steps(n, row, stair + add);
+
+}
 
 module.exports = steps;
