1b02c8c8cb7b6256a0c02859de844ae371a49b68
debjohnson33 <debjohnson_11@yahoo.com>
2018-04-02 14:38:46 -0500

    Finished fibonacci exercise

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,35 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn) {
+	const cache = {};
+	return function(...args) {
+		if (cache[args]) {
+			return cache[args];
+		} 
 
+		const result = fn.apply(this, args);
+		cache[args] = result;
+		return result;
+	};
+}
+function slowFib(n) { // Recursive solution
+	if (n < 2) {
+		return n;
+	}
+
+	return fib(n - 1) + fib(n - 2); //<-- Must subtract
+}									//2 in the second call
+const fib = memoize(slowFib); // Pass slowFib to									
+							// memoize to avoid repeated calls to fib for the same number
 module.exports = fib;
+
+// function fib(n) {  	// Iterative solution
+// 	const result = [0, 1];
+// 	for (let i = 2; i <= n; i++) {
+// 		const a = result[i - 1];
+// 		const b = result[i - 2];
+// 		result.push(a + b);
+// 	}
+// 	return result[n];
+// }
